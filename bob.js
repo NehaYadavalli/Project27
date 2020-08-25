e85db624405a1a2c00d7a0080030f16f485cfbd0
NehaYadavalli <68659585+NehaYadavalli@users.noreply.github.com>
2020-08-25 11:07:21 -0400

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,27 @@
+class Bob{
+
+    constructor(x,y,radius){
+
+        var options = {
+            isStatic: false,
+            restitution: 0.3,
+            friction: 0.5,
+            density: 1.2
+        }
+
+        this.body = Bodies.circle(x, y, radius, options);
+        World.add(world, this.body);}
+
+
+        display(){
+            var pos = this.body.position;
+    
+            fill ("pink");
+    
+            ellipseMode(CENTER);
+        
+            ellipse(pos.x,pos.y,100);
+    
+    
+        }
+    }
